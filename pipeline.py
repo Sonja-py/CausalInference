840a0d4ca591540b1a7f6266a8e60d593c27288c
Foundry (vector-client-token) <>
2023-12-04 03:28:27 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,9 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.4300054b-4092-4e59-b6a5-9418a642e834"),
+    final_data=Input(rid="ri.foundry.main.dataset.189cbacb-e1b1-4ba8-8bee-9d6ee805f498")
+)
+def unnamed(final_data):
+    
+
