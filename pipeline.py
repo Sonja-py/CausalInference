b4e5072f7903fe8897f9d1804e3ade9c9bf37cbf
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-01-03 21:11:32 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -276,7 +276,7 @@ def meta_learners_t(final_data):
         # rocs_r.append(roc)
         # ates_r.append(ate)
 
-        modelt2 = LogisticRegression(max_iter=1000, class_weight = class_weight_dict)
+        modelt2 = LogisticRegression(max_iter=1000, penalty=None, class_weight = class_weight_dict)
         learner_t2 = BaseTClassifier(learner = modelt2)
         learner_t2.fit(X=X_train, treatment=t_train, y=y_train)
         ite, yhat_cs, yhat_ts = learner_t2.predict(X=X_valid, treatment=t_valid, y=y_valid, return_components=True, verbose=True)
