a441d2c789a8ff9b9b481086c96214c0cc661c81
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-01-03 21:17:17 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -268,21 +268,13 @@ def meta_learners_t(final_data):
 
         # T-Learner
 
-        # modelt1 = RandomForestClassifier(n_estimators = 400, max_depth = 7, class_weight = class_weight_dict)
-        # learner_t1 = BaseTClassifier(learner = modelt1)
-        # learner_t1.fit(X=X_train, treatment=t_train, y=y_train)
-        # ite, yhat_cs, yhat_ts = learner_t1.predict(X=X_valid, treatment=t_valid, y=y_valid, return_components=True, verbose=True)
-        # roc, ate = metrics(y_valid, t_valid, ite, yhat_cs, yhat_ts, threshold, 'RandomForestClassifier')
-        # rocs_r.append(roc)
-        # ates_r.append(ate)
-
-        modelt2 = LogisticRegression(max_iter=1000, penalty='none', class_weight = class_weight_dict)
-        learner_t2 = BaseTClassifier(learner = modelt2)
-        learner_t2.fit(X=X_train, treatment=t_train, y=y_train)
-        ite, yhat_cs, yhat_ts = learner_t2.predict(X=X_valid, treatment=t_valid, y=y_valid, return_components=True, verbose=True)
-        roc, ate = metrics(y_valid, t_valid, ite, yhat_cs, yhat_ts, threshold, 'LogisticRegression')
-        rocs_l.append(roc)
-        ates_l.append(ate)
+        modelt1 = RandomForestClassifier(n_estimators = 400, max_depth = 7, class_weight = class_weight_dict)
+        learner_t1 = BaseTClassifier(learner = modelt1)
+        learner_t1.fit(X=X_train, treatment=t_train, y=y_train)
+        ite, yhat_cs, yhat_ts = learner_t1.predict(X=X_valid, treatment=t_valid, y=y_valid, return_components=True, verbose=True)
+        roc, ate = metrics(y_valid, t_valid, ite, yhat_cs, yhat_ts, threshold, 'RandomForestClassifier')
+        rocs_r.append(roc)
+        ates_r.append(ate)
 
         modelt2 = LogisticRegression(max_iter=1000, class_weight = class_weight_dict)
         learner_t2 = BaseTClassifier(learner = modelt2)
