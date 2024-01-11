2628e10a62939045555d7e402985e25dabea1772
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-01-11 22:15:33 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -194,119 +194,6 @@ def lr_slearner(final_data):
     return results_df
 
 @transform_pandas(
-    Output(rid="ri.foundry.main.dataset.c55c9d7f-ff97-450b-8d2c-fabecfaaa8ab"),
-    final_data=Input(rid="ri.foundry.main.dataset.189cbacb-e1b1-4ba8-8bee-9d6ee805f498")
-)
-def meta_learner_s(final_data):
-
-    def metrics(y_valid, t_valid, ite, yhat_cs, yhat_ts):
-        yhat_cs, yhat_ts = np.array(list(yhat_cs.values())[0]), np.array(list(yhat_ts.values())[0])
-        preds = (1. - t_valid) * yhat_cs + t_valid * yhat_ts
-        roc = roc_auc_score(y_valid, preds)
-        ate = ite.mean()
-        return roc, ate
-
-    def best_params_df(best_params, best_roc, best_ate, combination, model):
-        best_params['roc'] = best_roc
-        best_params['ate'] = best_ate
-        best_params['drug_0'] = combination[0]
-        best_params['drug_1'] = combination[1]
-        best_params['model'] = model
-        return pd.DataFrame(best_params, index=[0])
-
-    def grid_search(X_train, y_train, t_train, X_valid, y_valid, t_valid, class_weight_dict, model):
-        best_roc = 0.0
-        best_ate = 0.0
-        if model == 'RF':
-            estim_1 = [100, 200, 500]
-            estim_2 = ['gini', 'entropy', 'log_loss']
-            estim_3 = [3, 5, 7]
-        else:
-            estim_1 = ['l1', 'l2', 'elasticnet', None]
-            estim_2 = [0.1, 1, 10, 100]
-            estim_3 = [1000, 5000, 10000]
-        for crit_1 in estim_1:
-            for crit_2 in estim_2:
-                for crit_3 in estim_3:
-                    if model == 'RF':
-                        clf = RandomForestClassifier(n_estimators = crit_1, criterion = crit_2, max_depth = crit_3, class_weight=class_weight_dict)
-                    else:
-                        clf = LogisticRegression(penalty=crit_1, C=crit_2, max_iter=crit_3, solver='saga', class_weight=class_weight_dict)
-                    clf_learner = BaseSClassifier(learner = clf)
-                    clf_learner.fit(X=X_train, treatment=t_train, y=y_train)
-                    ite, yhat_cs, yhat_ts = clf_learner.predict(X=X_valid, treatment=t_valid, y=y_valid, return_components=True, verbose=True)
-                    roc, ate = metrics(y_valid, t_valid, ite, yhat_cs, yhat_ts)
-                    
-                    if roc > best_roc:
-                        best_ate = ate
-                        best_roc = roc
-                        # best_params = {'parameters': [('n_estimators', estimator), ('criterion', criterion), ('max_depth', depth)]}
-                        if model == 'RF':
-                            best_params = {'n_estimators': crit_1, 'criterion': crit_2, 'max_depth': crit_3, 'penalty':None, 'C':None, 'max_iter':None}
-                        else:
-                            best_params = {'n_estimators': None, 'criterion': None, 'max_depth': None, 'penalty':crit_1, 'C':crit_2, 'max_iter':crit_3}
-
-        return best_roc, best_ate, best_params
-
-    # Create and get the data for pair of different antidepressants
-    main_df = final_data.toPandas()
-    results_df = pd.DataFrame()
-    # ingredient_list = main_df.ingredient_concept_id.unique()
-    # ingredient_pairs = list(combinations(ingredient_list, 2))
-    initial_time = datetime.now()
-    ingredient_pairs = [(739138, 703547)]
-    threshold = 0.4
-    rocs_l = []
-    rocs_r = []
-    ates_r = []
-    ates_l = []
-
-    for idx, combination in enumerate(ingredient_pairs):
-        start_time = datetime.now()
-        print(f'-----------Running Meta-Learners for drug pair: {combination}. It is number {idx+1} of {len(ingredient_pairs)} -----------')
-        df = main_df.copy()
-        df = df[df.ingredient_concept_id.isin(list(combination))]
-        df['treatment'] = df['ingredient_concept_id'].apply(lambda x: 0 if x == combination[0] else 1)
-
-        X = df.drop(['person_id','severity_final', 'ingredient_concept_id', 'treatment'], axis=1)
-        y = df['severity_final']
-        t = df['treatment']
-
-        np.random.seed(0)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 2, stratify = y)
-        X_test, X_valid, y_test, y_valid = train_test_split(X_test, y_test, test_size = 0.5, random_state = 2, stratify = y_test)
-        y_train, y_valid, y_test = y_train.values, y_valid.values, y_test.values
-        
-        t_train = t[X_train.index]
-        t_train = t_train.values
-        t_test = t[X_test.index]
-        t_test = t_test.values
-        t_valid = t[X_valid.index]
-        t_valid = t_valid.values
-
-        class_weights = class_weight.compute_class_weight(class_weight = 'balanced', classes = np.unique(y), y = y)
-        class_weight_dict = dict(enumerate(class_weights))
-
-        best_roc, best_ate, best_params = grid_search(X_train, y_train, t_train, X_valid, y_valid, t_valid, class_weight_dict, 'RF')
-        print(f'RF - ROC: {best_roc}, {best_params}')
-        best_params_df = best_params_df(best_params, best_roc, best_ate, combination, 'RF')
-        results_df = pd.concat([results_df, best_params_df], ignore_index=True)
-        print('Time taken for RF', datetime.now()-start_time)
-    
-        # new_start_time = datetime.now()
-        # best_roc, best_ate, best_params = grid_search(X_train, y_train, t_train, X_valid, y_valid, t_valid, class_weight_dict, 'LR')
-        # print(f'LR - ROC: {best_roc}, {best_params}')
-        # best_params_df = best_params_df(best_params, best_roc, best_ate, combination, 'LR')
-        # results_df = pd.concat([results_df, best_params_df], ignore_index=True)
-        # print('Time taken for LR', datetime.now()-new_start_time)
-
-        print(f'Time taken for combination {idx+1} is {datetime.now() - start_time}')
-
-    print('Total time taken:',datetime.now() - initial_time)
-
-    return results_df
-
-@transform_pandas(
     Output(rid="ri.vector.main.execute.f2cebbba-3c15-4e6c-b89b-d8374a3b91f3"),
     final_data=Input(rid="ri.foundry.main.dataset.189cbacb-e1b1-4ba8-8bee-9d6ee805f498")
 )
@@ -446,6 +333,119 @@ def meta_learners_t(final_data):
 
     # print(val)
         
+
+@transform_pandas(
+    Output(rid="ri.foundry.main.dataset.c55c9d7f-ff97-450b-8d2c-fabecfaaa8ab"),
+    final_data=Input(rid="ri.foundry.main.dataset.189cbacb-e1b1-4ba8-8bee-9d6ee805f498")
+)
+def rf_slearner(final_data):
+
+    def metrics(y_valid, t_valid, ite, yhat_cs, yhat_ts):
+        yhat_cs, yhat_ts = np.array(list(yhat_cs.values())[0]), np.array(list(yhat_ts.values())[0])
+        preds = (1. - t_valid) * yhat_cs + t_valid * yhat_ts
+        roc = roc_auc_score(y_valid, preds)
+        ate = ite.mean()
+        return roc, ate
+
+    def best_params_df(best_params, best_roc, best_ate, combination, model):
+        best_params['roc'] = best_roc
+        best_params['ate'] = best_ate
+        best_params['drug_0'] = combination[0]
+        best_params['drug_1'] = combination[1]
+        best_params['model'] = model
+        return pd.DataFrame(best_params, index=[0])
+
+    def grid_search(X_train, y_train, t_train, X_valid, y_valid, t_valid, class_weight_dict, model):
+        best_roc = 0.0
+        best_ate = 0.0
+        if model == 'RF':
+            estim_1 = [100, 200, 500]
+            estim_2 = ['gini', 'entropy', 'log_loss']
+            estim_3 = [3, 5, 7]
+        else:
+            estim_1 = ['l1', 'l2', 'elasticnet', None]
+            estim_2 = [0.1, 1, 10, 100]
+            estim_3 = [1000, 5000, 10000]
+        for crit_1 in estim_1:
+            for crit_2 in estim_2:
+                for crit_3 in estim_3:
+                    if model == 'RF':
+                        clf = RandomForestClassifier(n_estimators = crit_1, criterion = crit_2, max_depth = crit_3, class_weight=class_weight_dict)
+                    else:
+                        clf = LogisticRegression(penalty=crit_1, C=crit_2, max_iter=crit_3, solver='saga', class_weight=class_weight_dict)
+                    clf_learner = BaseSClassifier(learner = clf)
+                    clf_learner.fit(X=X_train, treatment=t_train, y=y_train)
+                    ite, yhat_cs, yhat_ts = clf_learner.predict(X=X_valid, treatment=t_valid, y=y_valid, return_components=True, verbose=True)
+                    roc, ate = metrics(y_valid, t_valid, ite, yhat_cs, yhat_ts)
+                    
+                    if roc > best_roc:
+                        best_ate = ate
+                        best_roc = roc
+                        # best_params = {'parameters': [('n_estimators', estimator), ('criterion', criterion), ('max_depth', depth)]}
+                        if model == 'RF':
+                            best_params = {'n_estimators': crit_1, 'criterion': crit_2, 'max_depth': crit_3, 'penalty':None, 'C':None, 'max_iter':None}
+                        else:
+                            best_params = {'n_estimators': None, 'criterion': None, 'max_depth': None, 'penalty':crit_1, 'C':crit_2, 'max_iter':crit_3}
+                    print(f'Done - estimator_1 {crit_1}, estimator_2 {crit_2}, estimator_3 {crit_3}')
+        return best_roc, best_ate, best_params
+
+    # Create and get the data for pair of different antidepressants
+    main_df = final_data.toPandas()
+    results_df = pd.DataFrame()
+    # ingredient_list = main_df.ingredient_concept_id.unique()
+    # ingredient_pairs = list(combinations(ingredient_list, 2))
+    initial_time = datetime.now()
+    ingredient_pairs = [(739138, 703547)]
+    threshold = 0.4
+    rocs_l = []
+    rocs_r = []
+    ates_r = []
+    ates_l = []
+
+    for idx, combination in enumerate(ingredient_pairs):
+        start_time = datetime.now()
+        print(f'-----------Running Meta-Learners for drug pair: {combination}. It is number {idx+1} of {len(ingredient_pairs)} -----------')
+        df = main_df.copy()
+        df = df[df.ingredient_concept_id.isin(list(combination))]
+        df['treatment'] = df['ingredient_concept_id'].apply(lambda x: 0 if x == combination[0] else 1)
+
+        X = df.drop(['person_id','severity_final', 'ingredient_concept_id', 'treatment'], axis=1)
+        y = df['severity_final']
+        t = df['treatment']
+
+        np.random.seed(0)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 2, stratify = y)
+        X_test, X_valid, y_test, y_valid = train_test_split(X_test, y_test, test_size = 0.5, random_state = 2, stratify = y_test)
+        y_train, y_valid, y_test = y_train.values, y_valid.values, y_test.values
+        
+        t_train = t[X_train.index]
+        t_train = t_train.values
+        t_test = t[X_test.index]
+        t_test = t_test.values
+        t_valid = t[X_valid.index]
+        t_valid = t_valid.values
+
+        class_weights = class_weight.compute_class_weight(class_weight = 'balanced', classes = np.unique(y), y = y)
+        class_weight_dict = dict(enumerate(class_weights))
+
+        best_roc, best_ate, best_params = grid_search(X_train, y_train, t_train, X_valid, y_valid, t_valid, class_weight_dict, 'RF')
+        print(f'RF - ROC: {best_roc}, {best_params}')
+        best_params_df = best_params_df(best_params, best_roc, best_ate, combination, 'RF')
+        results_df = pd.concat([results_df, best_params_df], ignore_index=True)
+        print('Time taken for RF', datetime.now()-start_time)
+    
+        # new_start_time = datetime.now()
+        # best_roc, best_ate, best_params = grid_search(X_train, y_train, t_train, X_valid, y_valid, t_valid, class_weight_dict, 'LR')
+        # print(f'LR - ROC: {best_roc}, {best_params}')
+        # best_params_df = best_params_df(best_params, best_roc, best_ate, combination, 'LR')
+        # results_df = pd.concat([results_df, best_params_df], ignore_index=True)
+        # print('Time taken for LR', datetime.now()-new_start_time)
+
+        print(f'Time taken for combination {idx+1} is {datetime.now() - start_time}')
+
+    print('Total time taken:',datetime.now() - initial_time)
+
+    return results_df
 
 @transform_pandas(
     Output(rid="ri.vector.main.execute.3cc31dfe-610e-492d-924d-c5f6421324d1"),
