d354e010244d8a88fd2897cf7e4ca87575c2c025
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-01-12 04:18:10 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -115,34 +115,34 @@ def lr_slearner(final_data):
         l1_ratio = None
         estim_1 = ['l1', 'l2', 'elasticnet', 'none'] # penalty
         estim_2 = [0.1, 1, 10, 100] # C - regularization strength
-        # estim_3 = [1000, 5000, 10000] # max_iter
+        estim_3 = [1000, 5000, 10000] # max_iter
         estim_4 = ['lbfgs', 'newton-cg', 'saga'] # solver
         for crit_1 in estim_1:
             for crit_2 in estim_2:
-                # for crit_3 in estim_3:
-                for crit_4 in estim_4:
-                    # if crit_1 == 'none': crit_2 = 1
-                    if (crit_4 == 'lbfgs' and crit_1 == 'l1') or (crit_4 == 'lbfgs' and crit_1 == 'elasticnet'): continue
-                    if (crit_4 == 'newton-cg' and crit_1 == 'l1') or (crit_4 == 'newton-cg' and crit_1 == 'elasticnet'): continue
-                    if crit_1 == 'elasticnet': l1_ratio = 0.5
-                    clf = LogisticRegression(penalty=crit_1, C=crit_2, max_iter=1000, solver=crit_4, class_weight=class_weight_dict, l1_ratio=l1_ratio)
-                    clf_learner = BaseSClassifier(learner = clf)
-                    clf_learner.fit(X=X_train, treatment=t_train, y=y_train)
-                    ite, yhat_cs, yhat_ts = clf_learner.predict(X=X_valid, treatment=t_valid, y=y_valid, return_components=True, verbose=True)
-                    roc, ate = metrics(y_valid, t_valid, ite, yhat_cs, yhat_ts)
-                    
-                    if roc > best_roc:
-                        best_ate = ate
-                        best_roc = roc
-                        # best_params = {'parameters': [('n_estimators', estimator), ('criterion', criterion), ('max_depth', depth)]}
-                        best_params = {'n_estimators': np.nan, 
-                            'criterion': np.nan,
-                            'max_depth': np.nan,
-                            'penalty':crit_1,
-                            'C':crit_2,
-                            # 'max_iter':crit_3,
-                            'solver':crit_4}
-                    print(f'Done - penalty: {crit_1}, C: {crit_2}, solver: {crit_4}')
+                for crit_3 in estim_3:
+                    for crit_4 in estim_4:
+                        # if crit_1 == 'none': crit_2 = 1
+                        if (crit_4 == 'lbfgs' and crit_1 == 'l1') or (crit_4 == 'lbfgs' and crit_1 == 'elasticnet'): continue
+                        if (crit_4 == 'newton-cg' and crit_1 == 'l1') or (crit_4 == 'newton-cg' and crit_1 == 'elasticnet'): continue
+                        if crit_1 == 'elasticnet': l1_ratio = 0.5
+                        clf = LogisticRegression(penalty=crit_1, C=crit_2, max_iter=crit_3, solver=crit_4, class_weight=class_weight_dict, l1_ratio=l1_ratio)
+                        clf_learner = BaseSClassifier(learner = clf)
+                        clf_learner.fit(X=X_train, treatment=t_train, y=y_train)
+                        ite, yhat_cs, yhat_ts = clf_learner.predict(X=X_valid, treatment=t_valid, y=y_valid, return_components=True, verbose=True)
+                        roc, ate = metrics(y_valid, t_valid, ite, yhat_cs, yhat_ts)
+                        
+                        if roc > best_roc:
+                            best_ate = ate
+                            best_roc = roc
+                            # best_params = {'parameters': [('n_estimators', estimator), ('criterion', criterion), ('max_depth', depth)]}
+                            best_params = {'n_estimators': np.nan, 
+                                'criterion': np.nan,
+                                'max_depth': np.nan,
+                                'penalty':crit_1,
+                                'C':crit_2,
+                                'max_iter':crit_3,
+                                'solver':crit_4}
+                        print(f'Done - penalty: {crit_1}, C: {crit_2}, max_iter: {crit_3}, solver: {crit_4}')
         return best_roc, best_ate, best_params
 
     # Create and get the data for pair of different antidepressants
