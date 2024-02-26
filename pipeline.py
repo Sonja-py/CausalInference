b40b8f725902441f5756e2bea8c31241a4cf1b8a
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-02-26 02:51:08 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1036,8 +1036,12 @@ def unnamed_2():
             # weight_decay=args.weight_decay,
         )
 
-        print(cevae.model.y_mean(x_train, torch.zeros(x_train.size(0))))
+        y_0 = cevae.model.y_mean(x_train, torch.zeros(x_train.size(0))
+        y_1 = cevae.model.y_mean(x_train, torch.zeros(x_train.size(1))
         # print(y_train)
+        y = y_1 - y_0
+        print(f'y is {y})')
+        print(f'ite {(y_1 - y_0).mean(0)}')
 
         # Evaluate.
         # x_test, t_test, y_test, true_ite = generate_data()
