1c061513a898cc2106ac406d29131edcf5c66380
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2025-03-21 15:04:45 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1389,6 +1389,13 @@ def unnamed_2():
     return spark.createDataFrame([[]], schema=schema)
 
 @transform_pandas(
+    Output(rid="ri.vector.main.execute.a3ce013d-e251-43de-af2f-3d1c5bb1c4a1"),
+    test_lr_slearner=Input(rid="ri.foundry.main.dataset.67236741-6d93-418d-83c3-91a2b3ea8405")
+)
+def unnamed_3(test_lr_slearner):
+    
+
+@transform_pandas(
     Output(rid="ri.vector.main.execute.89021ed6-53c2-4027-8855-4b7e05f30b16"),
     final_data=Input(rid="ri.foundry.main.dataset.189cbacb-e1b1-4ba8-8bee-9d6ee805f498")
 )
