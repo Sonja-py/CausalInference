dd81298acebe5284fdd8fb17265d8d37e453e8c5
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2025-03-25 07:03:34 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -908,7 +908,7 @@ def rf_tlearner_predictions_y0y1(final_data, Test_rf_tlearner):
         results_df = pd.concat([results_df, temp_df], ignore_index=True)
         # spark_df = spark.createDataFrame(results_df.astype({'yhat_ts': 'int'}))
 
-        return spark_df
+        return results_df
         print(f'Time taken for combination {idx+1}: {datetime.now() - start_time}')
     
     print('Total time taken:', datetime.now() - initial_time)
